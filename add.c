92a813f88a86267c84875ee70c73310c39310e75
Jowey <50485764+joweymwesh@users.noreply.github.com>
2023-07-22 13:35:45 +0300

    Update add.c

--- add.c
+++ add.c
@@ -5,13 +5,14 @@ int main(void)
 {
 	int a = 4;
 	int b = 3;
+	pid_t ppid;
+	
 	int sum = a + b;
-	pid_t pid;
-
-	pid = getpid();
-
+	
 	printf("sum is %d\n", sum);
-	printf("pid is %u\n", pid);
+	
+	ppid = getppid();
+	printf("ppid is %u\n", pid);
 
 	return 0;
 }
